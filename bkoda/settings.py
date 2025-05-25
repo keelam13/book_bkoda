5266aca72c2b1b72d84ac4d2927bd5582fdafa21
Keevin Aroco <gavikde2022@gmail.com>
2025-05-25 20:12:29 +0200

    complete home page header and css

--- bkoda/settings.py
+++ bkoda/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Directory for static files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory for uploaded media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
