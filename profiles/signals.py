b840e1f5d024847cb28a718cc73550b651281a97
Keevin Aroco <gavikde2022@gmail.com>
2025-06-05 22:41:01 +0200

    Add booking list to profile template

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,4 +1,3 @@
-
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
@@ -13,7 +12,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    try:
-        instance.profile.save()
-    except UserProfile.DoesNotExist:
-        UserProfile.objects.create(user=instance)
+    instance.profile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,26 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import UserProfile
 from .forms import ProfileForm
 
+from booking.models import Booking
+
 @login_required
 def profile(request):
-    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        form = ProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile was successfully updated!')
-            return redirect('profiles:profile')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
         form = ProfileForm(instance=profile)
+    bookings = profile.user.booking_set.all().order_by('-booking_date')
 
     context = {
-        'profile': profile,
         'form': form,
+        'bookings': bookings,
     }
     return render(request, 'profiles/profile.html', context)
+
+def order_history(request, booking_reference):
+    """
+    Render the user's booking history page.
+    """
+    booking = get_object_or_404(Booking, booking_reference=booking_reference)
+
+    messages.info(request, (
+        f'This is a past confirmation for booking number {booking_reference}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'booking/booking_success.html'
+    context = {
+        'booking': booking,
+    }
+
+    return render(request, template, context)
