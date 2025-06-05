837e463a81a992fdc4f0c1db353e2e8bf0f8c148
Keevin Aroco <gavikde2022@gmail.com>
2025-06-05 19:11:23 +0200

    Modify profile display, form and model,

--- profiles/models.py
+++ profiles/models.py
@@ -5,14 +5,14 @@ from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, blank=False, null=False)
-    email = models.EmailField(max_length=20, blank=False, null=False)
-    street_address1 = models.TextField(max_length=20, blank=False, null=False)
-    street_address2 = models.TextField(max_length=20, blank=False, null=False)
-    city = models.CharField(max_length=100, blank=False, null=False)
-    postcode = models.CharField(max_length=20, blank=False, null=False)
-    country = CountryField(blank_label='Country *', blank=False, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    default_phone_number = models.CharField(max_length=15, blank=False, null=False)
+    default_email = models.EmailField(max_length=20, blank=False, null=False)
+    default_street_address1 = models.TextField(max_length=20, blank=False, null=False)
+    default_street_address2 = models.TextField(max_length=20, blank=False, null=False)
+    default_city = models.CharField(max_length=100, blank=False, null=False)
+    default_postcode = models.CharField(max_length=20, blank=False, null=False)
+    default_country = CountryField(blank_label='Country *', blank=False, null=False)
 
     def __str__(self):
         return self.user.username
