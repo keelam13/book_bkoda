0687cf3845b939ff1f546d13c78d44e563fa0cec
Keevin Aroco <gavikde2022@gmail.com>
2025-08-26 12:26:47 +0200

    Edit back-to-top button display, remove from other htmls

--- static/js/base.js
+++ static/js/base.js
@@ -10,7 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /** Back to Top Button Logic */
     const backToTopButton = document.getElementById('back-to-top');
+    const scrollThreshold = 300;
+
     if (backToTopButton) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > scrollThreshold) {
+                backToTopButton.style.display = 'block';
+            } else {
+                backToTopButton.style.display = 'none';
+            }
+        });
+
         backToTopButton.addEventListener('click', function (e) {
             e.preventDefault();
             window.scrollTo({ top: 0, behavior: 'smooth' });
