a8090ef306cfc46f02c0e60f5f4c74c3ba17c1cb
Keevin Aroco <gavikde2022@gmail.com>
2025-11-16 23:28:14 +0100

    Add phone number regex validator

--- my_account/models.py
+++ my_account/models.py
@@ -13,13 +13,25 @@ postal_code_validator = RegexValidator(
     code='invalid_postal_code'
 )
 
+PHONE_REGEX = r'^\+?\d{9,15}$' 
+
+phone_number_validator = RegexValidator(
+    regex=PHONE_REGEX,
+    message='Phone number must be entered in international format (e.g., +63917xxxxxxx) and be between 9 and 15 digits.',
+    code='invalid_phone_number'
+)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
     default_name = models.CharField(max_length=50, blank=True, null=True)
     default_phone_number = models.CharField(
-        max_length=15, blank=True, null=True)
+        max_length=15, 
+        blank=True, 
+        null=True,
+        validators=[phone_number_validator]
+    )
     default_email = models.EmailField(max_length=254, blank=True, null=True)
     default_street_address1 = models.CharField(
         max_length=225, blank=True, null=True)
